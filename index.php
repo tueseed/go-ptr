f6888715c60262f75b341e3d82bb6a181e206a12
Nutthapong <tue_seed@hotmail.com>
2019-03-17 17:43:35 +0700

    17052562

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<script src="js/jquery.min.js"></script>
-		<script src="js/popper.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<div class="jumbotron text-center">
