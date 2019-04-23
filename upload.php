5f7a82c1838e579198e803f68b05e4da26cf0561
Nutthapong <tue_seed@hotmail.com>
2019-04-23 19:58:58 +0700

    adadasd

--- upload.php
+++ upload.php
@@ -5,19 +5,8 @@
     $name = $_FILES["images"]["name"][$key];
     move_uploaded_file( $_FILES["images"]["tmp_name"][$key], "uploads/" . $_FILES['images']['name'][$key]);
   }
-}
-move_uploaded_file( $_FILES["images"]["tmp_name"], "images/" . basename($_FILES['images']['name']));
-$file = $_FILES["images"]["tmp_name"];
+}*/
+move_uploaded_file( $_FILES["tmp_name"], "images/" . basename($_FILES['name']));
+$file = $_FILES["tmp_name"];
  
-echo "<h2>".$file."</h2>";*/
-if(!empty($_FILES['images']))
-{
-  $path = "images/";
-  $path = $path . basename( $_FILES['images']['name']);
-  if(move_uploaded_file($_FILES['images']['tmp_name'], $path)) {
-    echo "The file ".  basename( $_FILES['images']['name']). 
-    " has been uploaded";
-  } else{
-      echo "There was an error uploading the file, please try again!";
-  }
-}
+echo "<h2>".$file."</h2>";
