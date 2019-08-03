9ce1bb5121c1f25364433815cba5c974e6fe739f
tueseed <tue_seed@hotmail.com>
2019-08-03 21:23:34 +0700

    cxzcxc

---
+++ learn.php
@@ -0,0 +1,20 @@
+<div class="row mt-3 ">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="text-center">
+            <label class="font-weight-bold bg-light py-2 px-5 shadow-sm" style="font-size:18px;border-radius: 20px;color:#cce800;">
+                <i class="fas fa-graduation-cap"></i>
+                <span>การอบรมให้ความรู้</span>
+            </label>
+        </div>
+
+        
+        <p style="font-size:16px;" class="mt-2"> 
+            <span class="pl-1" >
+            กฟอ.โพธารามมีการอบรมเกี่ยวกับ Green Office ช่วงปลายเดือน สิงหาคม 2562 โดยมี คณะวิทยากรจากทางเขต กฟต.1ที่ได้รับใบรับรองจากหน่วยงานภายนอกมาอบรมให้ นอกจากการอบรมเรื่อง Green Office แล้ว โพธารามยังมีการจัดอบรมเรื่องการซ้อมดับเพลิงและอพยพหนีไฟด้วย
+            </span>
+        </p>
+        
+
+        
+</div>
+</div>
