685b31e1c5c22a60c97cfab5662a10ba98b2310d
tueseed <tue_seed@hotmail.com>
2019-08-03 10:34:52 +0700

    sdaasdas

--- firstbot.php
+++ firstbot.php
@@ -73,6 +73,43 @@
 		}
 	}
 
+	function rich_default($uid)
+	{
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.line.me/v2/bot/user/".$uid."/richmenu",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "DELETE",
+		CURLOPT_HTTPHEADER => array(
+			"Accept: */*",
+			"Accept-Encoding: gzip, deflate",
+			"Authorization: Bearer pwo0kxwpp2MFook0bX8Gr+XR+cOuT5/4mCU2aKFMa5ML5V9PDZAAqaRS5uyPGBC4DS2lkzekn1nT8OBpjE8HPNQqXJm6mBcixaVfmtLm08N1kug5XMgHL4CZAaKXSyOUZumGYOnEOjw7VSx0llzYtgdB04t89/1O/w1cDnyilFU=",
+			"Cache-Control: no-cache",
+			"Connection: keep-alive",
+			"Content-Length: ",
+			"Host: api.line.me",
+			"Postman-Token: 001edba2-c6ed-4477-b26f-7c177e70a014,a6b8688f-3ee4-46e4-a60c-e11b26c25203",
+			"User-Agent: PostmanRuntime/7.15.2",
+			"cache-control: no-cache"
+		),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+		echo "cURL Error #:" . $err;
+		} else {
+		echo $response;
+		}
+	}
+
 	function reply_msg($txtin,$replyToken)//สร้างข้อความและตอบกลับ
 	{
 		$access_token = 'pwo0kxwpp2MFook0bX8Gr+XR+cOuT5/4mCU2aKFMa5ML5V9PDZAAqaRS5uyPGBC4DS2lkzekn1nT8OBpjE8HPNQqXJm6mBcixaVfmtLm08N1kug5XMgHL4CZAaKXSyOUZumGYOnEOjw7VSx0llzYtgdB04t89/1O/w1cDnyilFU=';
@@ -113,7 +150,11 @@
 				}
 				else if($txtin == "rich3")
 				{
-
+					rich2($uid);
+				}
+				else if($txtin == "rich1")
+				{
+					rich_default($uid);
 				}
 
 				// reply_msg($uid,$replyToken);      
--- testrich1.php
+++ testrich1.php
@@ -1,14 +1,22 @@
-
 <?php
-    $url = 'https://api.line.me/v2/bot/user/Ua9ba6c25071c19588c095ec147efe2b1/richmenu/richmenu-a8b364e53db970d5eebc86e60a947fcf';
-    $access_token = 'pwo0kxwpp2MFook0bX8Gr+XR+cOuT5/4mCU2aKFMa5ML5V9PDZAAqaRS5uyPGBC4DS2lkzekn1nT8OBpjE8HPNQqXJm6mBcixaVfmtLm08N1kug5XMgHL4CZAaKXSyOUZumGYOnEOjw7VSx0llzYtgdB04t89/1O/w1cDnyilFU=';
-    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    echo $result . "\r\n";
-?>
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://api.line.me/v2/bot/user/Ua9ba6c25071c19588c095ec147efe2b1/richmenu/richmenu-6970e1b6ab46d6d4a5d8657bb5208560",
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer pwo0kxwpp2MFook0bX8Gr+XR+cOuT5/4mCU2aKFMa5ML5V9PDZAAqaRS5uyPGBC4DS2lkzekn1nT8OBpjE8HPNQqXJm6mBcixaVfmtLm08N1kug5XMgHL4CZAaKXSyOUZumGYOnEOjw7VSx0llzYtgdB04t89/1O/w1cDnyilFU=",
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
