cef0a013b3125c080db048b8292a15c3ada04b67
tueseed <tue_seed@hotmail.com>
2019-08-03 19:22:31 +0700

    casdas

---
+++ kanatamngan.php
@@ -0,0 +1,48 @@
+<div class="row mt-3 ">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="text-center">
+            <label class="font-weight-bold bg-light py-2 px-5 shadow-sm" style="font-size:18px;border-radius: 20px;color:#cce800;">
+                <i class="fas fa-users"></i>
+                <span>คณะทำงาน</span>
+            </label>
+        </div>
+
+        <p style="font-size:16px;" class="mt-2"> 
+            <span class="pl-1 font-weight-bold" >
+                คณะทำงานมีดังนี้
+            </span>
+        </p>
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+                หมวด 1 แผนกบัญชีฯ
+            </span>
+        </p>
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+            หมวด 2 แผนกก่อสร้าง
+            </span>
+        </p>
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+            หมวด 3 แผนกบริการฯ
+            </span>
+        </p>
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+            หมวด 4 แผนกมิเตอร์
+            </span>
+        </p>
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+            หมวด 5 แผนกปฏิบัติการฯ
+            </span>
+        </p>
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+            หมวด 6 แผนกบริหาร+คลังพัสดุ
+            </span>
+        </p>
+
+        
+</div>
+</div>
