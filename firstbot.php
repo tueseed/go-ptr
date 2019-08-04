151ea5fd8613d3c17ec6d95cbfd18521481897a4
tueseed <tue_seed@hotmail.com>
2019-08-04 10:51:39 +0700

    asdasd

--- firstbot.php
+++ firstbot.php
@@ -44,6 +44,50 @@
 		curl_close($ch);
 		echo $result . "\r\n";
 	}
+
+	function button($replyToken)
+	{
+		$access_token = 'pwo0kxwpp2MFook0bX8Gr+XR+cOuT5/4mCU2aKFMa5ML5V9PDZAAqaRS5uyPGBC4DS2lkzekn1nT8OBpjE8HPNQqXJm6mBcixaVfmtLm08N1kug5XMgHL4CZAaKXSyOUZumGYOnEOjw7VSx0llzYtgdB04t89/1O/w1cDnyilFU=';
+		// $messages = ['type' => 'text','text' => $txtin];//สร้างตัวแปร 
+		$messages = '{
+						"type": "template",
+						"altText": "this is a buttons template",
+						"template": {
+			  							"type": "buttons",
+			  							"actions": [
+													{
+														"type": "uri",
+														"label": "รายละเอียด",
+														"uri": "line://app/1561756390-QMx74VGA"
+													}
+											],
+												],
+			  							"thumbnailImageUrl": "https://go-ptr.herokuapp.com/images/classroom.JPG",
+										  "title": "การจัดอบรม",
+										  "text": "การจัดการอบรม กฟอ.โพธาราม"
+									}
+		  			}';
+		$mes = json_decode($messages);			  
+		$url = 'https://api.line.me/v2/bot/message/reply';
+		$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$mes],
+				];
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		echo $result . "\r\n";
+
+
+	}
+
 	function rich2($uid)
 	{
 		$access_token = 'pwo0kxwpp2MFook0bX8Gr+XR+cOuT5/4mCU2aKFMa5ML5V9PDZAAqaRS5uyPGBC4DS2lkzekn1nT8OBpjE8HPNQqXJm6mBcixaVfmtLm08N1kug5XMgHL4CZAaKXSyOUZumGYOnEOjw7VSx0llzYtgdB04t89/1O/w1cDnyilFU=';
@@ -140,7 +184,7 @@
 				}
 				else if($txtin == "การจัดอบรม")
 				{
-					imagemap($replyToken);
+					button($replyToken);
 				}
 				// reply_msg($uid,$replyToken);      
 			}
