5da1065ffc0d9fa0e65db67d10c631f2cde647a5
tueseed <tue_seed@hotmail.com>
2019-08-04 16:19:11 +0700

    dfsfsef

---
+++ commu.php
@@ -0,0 +1,59 @@
+<div class="row mt-3 ">
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="text-center">
+            <label class="font-weight-bold bg-light py-2 px-5 shadow-sm" style="font-size:18;border-radius: 20px;color:#cce800;">
+                <i class="fas fa-comment"></i>
+                <span>ช่องทางการสื่อสาร</span>
+            </label>
+        </div>
+
+        
+        <p style="font-size:16px;" class="font-weight-bold mt-2"> 
+            <span class="pl-1" >
+            เราสามารถรับข่าวสารได้จากช่องทางต่างๆดังนี้ 
+            </span>
+        </p>
+
+        <p style="font-size:16px;" class="font-weight-bold mt-2"> 
+            <span class="pl-1" >
+            Line
+            </span>
+        </p>
+        <p style="font-size:16px;" class="mt-2"> 
+            <span class="pl-1" >
+            กลุ่มไลน์ กฟอ.พธร. ข่าวสาร มีระบบ Notify แจ้งเตือน และประชาสัมพันธ์ข้อมูลข่าวสารต่างๆ
+            </span>
+        </p>
+        <p style="font-size:16px;" class="mt-2"> 
+            <span class="pl-1" >
+           Line Bot Mr.Green แหล่งข้อมูลความรู้ด้านสิ่งแวดล้อมและแบบทดสอบ
+            </span>
+        </p>
+
+        <p style="font-size:16px;" class="font-weight-bold mt-2"> 
+            <span class="pl-1" >
+            Facebook
+            </span>
+        </p>
+
+        <p style="font-size:16px;" class="mt-2"> 
+            <span class="pl-1" >
+          กฟอ.โพธาราม ได้จัดตั้ง Facebook Page เพื่อใช้ในการสื่อสารข้อมูลข่าวสารด้านสิ่งแวดล้อมให้กับพนักงาน
+            </span>
+        </p>
+
+        <a href="https://www.facebook.com/%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%84%E0%B8%9F%E0%B8%9F%E0%B9%89%E0%B8%B2%E0%B8%AA%E0%B9%88%E0%B8%A7%E0%B8%99%E0%B8%A0%E0%B8%B9%E0%B8%A1%E0%B8%B4%E0%B8%A0%E0%B8%B2%E0%B8%84%E0%B8%AD%E0%B8%B3%E0%B9%80%E0%B8%A0%E0%B8%AD%E0%B9%82%E0%B8%9E%E0%B8%98%E0%B8%B2%E0%B8%A3%E0%B8%B2%E0%B8%A1-403470770501052/"><img src="images/facebook.jpg"></a>
+
+        <p style="font-size:16px;" class="font-weight-bold mt-3"> 
+            <span class="pl-1" >
+            บอร์ดประชาสัมพันธ์
+            </span>
+        </p>
+
+        <p style="font-size:16px;" class="font-weight-bold mt-2"> 
+            <span class="pl-1" >
+            สารบรรณอิเล็กทรอนิกส์ 
+            </span>
+        </p>     
+</div>
+</div>
