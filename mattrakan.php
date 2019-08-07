27c330a058ab0bebb10edb6940c343d248d18cf6
tueseed <tue_seed@hotmail.com>
2019-08-08 02:19:10 +0700

    ปผผ)

--- mattrakan.php
+++ mattrakan.php
@@ -1,8 +1,8 @@
 <div class="row mt-3 ">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <div class="text-center">
-            <label class="font-weight-bold bg-light py-2 px-5 shadow-sm" style="font-size:18px;border-radius: 20px;color:#cce800;">
-                <i class="fas fa-industry"></i>
+            <label class="font-weight-bold bg-light py-2 px-5 shadow-sm" style="font-size:16px;border-radius: 20px;color:#cce800;">
+                <i class="fas fa-chart-line"></i>
                 <span>เป้าหมายและมาตรการด้านสิ่งแวดล้อม</span>
             </label>
         </div>
@@ -10,9 +10,45 @@
         
         
 
-        <p style="font-size:16px;" class="font-weight-bold  mt-2"> 
+        <p style="font-size:16px;" > 
             <span class="pl-1" >
-            1.ฉลากเขียว
+            1.การใช้พลังงานไฟฟ้า ให้พิ่มขึ้นได้ไม่เกิน 4.63 % จากปริมาณค่าเฉลี่ยการใช้พลังงานไฟฟ้าปี 2561
+            </span>
+        </p>
+
+        <p style="font-size:16px;" > 
+            <span class="pl-1" >
+            2.การใช้น้ำมันเชื้อเพลิง ให้ลดลง 5 % จากปริมาณค่าเฉลี่ยการใช้น้ำมันเชื้อเพลิงของปี 2561
+            </span>
+        </p>
+
+        <p style="font-size:16px;" > 
+            <span class="pl-1" >
+            3.การใช้น้ำประปา ให้เท่ากับปริมาณค่าเฉลี่ยการใช้น้ำประปาปี 2561 
+            </span>
+        </p>
+
+        <p style="font-size:16px;" > 
+            <span class="pl-1" >
+            4.การใช้กระดาษ ให้ลดลง 40 % จากปริมาณการใช้กระดาษปี 2560
+            </span>
+        </p>
+
+        <p style="font-size:16px;" > 
+            <span class="pl-1" >
+            5.ปริมาณของเสีย ให้นำกลับมาใช้ประโยชน์ได้ไม่น้อยกว่า 45 %  ของปริมาณของเสียในแต่ละเดือน
+            </span>
+        </p>
+
+        <p style="font-size:16px;" > 
+            <span class="pl-1" >
+            6.ปริมาณการใช้เครื่องเขียน ให้ใช้ตามมาตรการ 5 ส.
+            </span>
+        </p>
+
+        <p style="font-size:16px;" > 
+            <span class="pl-1" >
+            7.ปริมาณก๊าซเรือนกระจก ให้เพิ่มขึ้นได้ไม่เกิน 3 % เปรียบเทียบจากปี 2561
             </span>
         </p>
 
