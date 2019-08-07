120513c8e4554106621394b30ad06b079a3f4186
tueseed <tue_seed@hotmail.com>
2019-08-08 01:54:06 +0700

    หกฟกหก

--- fire.php
+++ fire.php
@@ -12,6 +12,53 @@
             พนักงานทุกคนต้องใช้ถังดับเพลิงได้  พร้อมทั้งอธิบายขั้นตอนการอพยพได้ด้วย
             </span>
         </p>
+        <p style="font-size:16px;" class="font-weight-bold mt-2"> 
+            <span class="pl-1" >
+                1.ขั้นตอนการใช้ถังดับเพลิง
+            </span>
+        </p>
         <img src="images/use-fire.jpg" class="mt-2">
+
+        <p style="font-size:16px;" class="font-weight-bold mt-2"> 
+            <span class="pl-1" >
+                2.ขั้นตอนการอพยพ
+            </span>
+        </p>
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+                เมื่อสัญญาณเตือนภัยดังขึ้น
+            </span>
+        </p>
+
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+                1.ให้ทุกคนกลับเข้าที่นั่งรอฟังคำสั่งอพยพ
+            </span>
+        </p>
+
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+                2.พนง.เวรระงับอัคคีภัยเดินตรวจตามชั้นตัวเอง
+            </span>
+        </p>
+
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+                3.เวรระงับอัคคีภัยสั่งการอพยพ และนำทางไปที่จุดรวมพล
+            </span>
+        </p>
+
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+                4.แต่ละชั้นตรวจสอบสมาชิกของตัวเอง
+            </span>
+        </p>
+
+        <p style="font-size:16px;"> 
+            <span class="pl-1" >
+                5.รายงาน ผจก. 
+            </span>
+        </p>
+        
 </div>
 </div>
